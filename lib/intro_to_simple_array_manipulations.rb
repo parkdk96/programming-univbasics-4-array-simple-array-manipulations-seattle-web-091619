270f6f1cd356513cfe3e633a210245b223df34c8
Daniel Park <parkdk96@gmail.com>
2019-08-23 05:55:58 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+def using_push(array, string)
+  return array.push(string)
+end
+
+def using_unshift(array, string)
+  return array.unshift(string)
+end
+
+def using_pop(array)
+  return array.pop 
+end
+
+def pop_with_args(array)
+  return array.pop(2)
+end
+
